1e61d75a5bfe97528ddc7c9b22336d2215ff8aec
leonel-giuliano <129548024+leonel-giuliano@users.noreply.github.com>
2024-10-30 15:23:48 -0300

    Listen for clients

---
+++ include/server.h
@@ -0,0 +1,9 @@
+#ifndef __BASH_WSP_SERVER_H__
+#define __BASH_WSP_SERVER_H__
+
+
+#define SERV_PORT       18000
+#define SERV_BACKLOG    20
+
+
+#endif
---
+++ server/server.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+#include "errnum.h"
+
+
+int main(void) {
+    int                 e = 0;
+    int                 listenfd;
+    struct sockaddr_in  servaddr;
+
+    errnum_program_name = "bash-wsp";
+
+
+    puts("Creating socket...");
+    if((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+        return errnum(ERR_NEW_SCK);
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(SERV_PORT);
+
+    puts("Binding socket...");
+    if(bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr))) {
+        e = errnum(ERR_BIND);
+        goto close_listenfd;
+    }
+
+    puts("Opening socket to listen...");
+    if(listen(listenfd, SERV_BACKLOG)) {
+        e = errnum(ERR_LISTEN);
+        goto close_listenfd;
+    }
+
+
+    close_listenfd:
+        if(close(listenfd)) e = errnum(ERR_CLOSE_SCK);
+
+        return e;
+}
